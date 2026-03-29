d0d9d5346e24e22226aa0da127f84f5d0d9c314f
Nitesh Kumar <nk183sharma@gmail.com>
2026-03-29 09:44:28 +0530

    Time: 0 ms (100%), Space: 43.1 MB (53.55%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,48 +1,57 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+        
 
         int left=0;
         int right=matrix[0].length-1;
         int top=0;
         int bottom=matrix.length-1;
-        ArrayList<Integer> list = new ArrayList<>();
-        while ( top <= bottom && left<=right){
-           
-            //left to right                
-            for(int i=left; i <=right  ; i++ ){
-                list.add(matrix[top][i]);
-            } 
+        ArrayList<Integer> list=new ArrayList<>();
+
+        while (top<=bottom && left <= right){
+            // left to right
+            for (int i=left; i<=right; i++){
+             list.add(matrix[top][i]);
+
+            }
             top++;
 
-            // top to bottom
-            for( int i= top   ; i<= bottom ; i++ ) {
-                 list.add( matrix[i][right]);
+            //top to bottom
+            for (int i=top; i<= bottom ; i++){
+                list.add(matrix[i][right]);
+
+
             }
             right--;
 
-            // right to left
-            if (top <= bottom) {
-            for( int i=right; i >= left ; i--  ){
+            //right to left
+
+           if (top<=bottom){
+
+            for(int i= right; i>=left; i--){
+
                 list.add(matrix[bottom][i]);
 
             }
-            }
             bottom--;
-             if (left <= right) {
-            // bottom to top
-            for( int i= bottom  ; i>=top ; i-- ){
+            }
+
+
+             if (left<= right){
+            //bottom to top
+            for(int i=bottom ; i>=top ;i--){
+
                 list.add(matrix[i][left]);
+
+
             }
             left++;
+           }
+
+
 
 
         }
-        
-        
+        return list;
     }
-    return list;
 }
-}
-
-
-  
