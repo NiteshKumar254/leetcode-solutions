f3358b2c2a2398e2e8d1ae587980508c18145032
Nitesh Kumar <nk183sharma@gmail.com>
2026-04-07 17:10:03 +0530

    Time: 1 ms (95.39%), Space: 43.3 MB (77.95%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
 
-
-        List<List<Integer>> list= new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
 
         for(int i=0; i<numRows; i++){
             List<Integer> temp= new ArrayList<>();
-              for(int j=0; j<=i; j++){
-                temp.add(findComb(i,j));         
-            }
-            list.add(temp);
-        }
-        return list;   
-    }
 
-    public static int findComb(int n, int r){
+            for(int j=0; j<=i;j++){
+                temp.add(findComb(i,j));
+
+            }
+            ans.add(temp);
+        }
+        return  ans;
+    }
+        
+        public static int findComb(int n, int r){
         int ans=1;
         for(int i=0;i<r; i++){
-            ans=ans*(n-i);
-            ans=ans/(i+1);
+           ans=ans*(n-i);
+           ans=ans/(i+1);
         }
         return ans;
     }
