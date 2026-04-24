777c3653e5110405c9420efc67fa360e227586f9
Nitesh Kumar <nk183sharma@gmail.com>
2026-04-24 12:55:58 +0530

    Time: 0 ms (100%), Space: 43.5 MB (93.45%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int matrix2[][]=new int[matrix.length][matrix[0].length];
+
+
+        for(int i=0; i<matrix.length ; i++){
+            for(int j=0; j<matrix[0].length ; j++ ){
+
+                matrix2[j][(matrix.length-1)-i]=matrix[i][j];
+            }
+
+        }
+           for(int i=0; i<matrix.length ; i++){
+            for(int j=0; j<matrix[0].length ; j++ ){
+
+                matrix[i][j]=matrix2[i][j];
+            }
+
+        }
+
+
+
+     
+
+
+
+    
+
+    
+
+
+
+        
+    }
+}
